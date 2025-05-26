c7067b8ee4c64c2b32d2e87a849fa4ba19f8c22f
LathaK-Adappt <latha.k@adappt.ai>
2025-05-26 21:24:41 +0530

    update images and data

---
+++ src/Utils/Data.js
@@ -0,0 +1,69 @@
+export const fruits = [
+    {
+    name: "banana",
+    weight: "1kg",
+    price: 30,
+    img: require('../assets/veg1.png'),
+  },
+  {
+    name: "apple",
+    weight: "1kg",
+    price: 250,
+   img: require('../assets/veg2.png'),
+  },
+  {
+    name: "orange",
+    weight: "1kg",
+    price: 40,
+   img: require('../assets/veg3.png'),
+  },
+  {
+    name: "pine apple",
+    weight: "1kg",
+    price: 60,
+    img: require('../assets/apple.png'),
+  },
+];
+
+export const vegetables = [
+  {
+    name: "tomoto",
+    weight: "1kg",
+    price: 30,
+    img: "https://w7.pngwing.com/pngs/891/45/png-transparent-tomato-tomato-fresh-fruits-thumbnail.png",
+  },
+  {
+    name: "Green Chilli",
+    weight: "1kg",
+    price: 40,
+    img: "https://w7.pngwing.com/pngs/199/527/png-transparent-mirchi-ka-salan-chili-pepper-leaf-vegetable-serrano-pepper-hot-peppers-natural-foods-leaf-vegetable-food-thumbnail.png",
+  },
+  {
+    name: "cabbage",
+    weight: "1kg",
+    price: 45,
+    img: "https://w7.pngwing.com/pngs/548/1000/png-transparent-green-cabbage-cauliflower-red-cabbage-white-cabbage-vegetable-savoy-cabbage-cabbage-leaf-vegetable-food-leaf-thumbnail.png",
+  },
+  {
+    name: "onion",
+    weight: "1kg",
+    price: 50,
+    img: "https://w7.pngwing.com/pngs/788/438/png-transparent-rd-onions-red-onion-shallot-vegetable-organic-food-white-onion-onion-food-nutrition-wholesale-thumbnail.png",
+  },
+];
+
+export const Dropdown = [
+  {
+    title: "Product-Detail",
+    content:
+      "Apples are Nutritious. Apples May Be Good For Weight Loss. Apples May Be Good For Your Heart. As Part Of A Healtful And Varied Diet.",
+  },
+  {
+    title: "Nutritions",
+    content: "Vitamin12",
+  },
+  {
+    title: "Reviews",
+    content: "4-reviews found in this product review",
+  },
+];
